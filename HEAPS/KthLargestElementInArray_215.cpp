eb36d53e45335ba9ed4b360287756a314e8bfaed
rishikarohila12 <ruhelarishika@gmail.com>
2024-02-16 13:53:40 +0530

    initial commit

---
+++ HEAPS/KthLargestElementInArray_215.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
+        }
+        return pq.top();
+    }
+};
