7e03b9468055ed3469e799c239f7d0893199a5cc
rishikarohila12 <ruhelarishika@gmail.com>
2024-02-17 12:50:35 +0530

    initial commit

---
+++ HEAPS/FindKclosestElement_658.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    typedef pair<int, int> p;
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<p> pq;
+        for (auto ele : arr) {
+            int dis = abs(x - ele);
+            pq.push({dis, ele});
+            if (pq.size() > k)
+                pq.pop();
+        }
+        vector<int> ans;
+        while (pq.size() > 0) {
+            int ele = pq.top().second;
+            ans.push_back(ele);
+            pq.pop();
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
